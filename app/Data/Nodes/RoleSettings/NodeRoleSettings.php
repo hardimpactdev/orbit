231abfb2c3db8eb526105a55ec006c8250666f88
nckrtl <nick.retel@gmail.com>
2026-05-17 10:51:43 +0200

    fix: declare role settings factory contract

--- app/Data/Nodes/RoleSettings/NodeRoleSettings.php
+++ app/Data/Nodes/RoleSettings/NodeRoleSettings.php
@@ -7,6 +7,11 @@ namespace App\Data\Nodes\RoleSettings;
 interface NodeRoleSettings
 {
     /**
+     * @param  array<string, mixed>  $settings
+     */
+    public static function fromArray(array $settings): self;
+
+    /**
      * @return array<string, mixed>
      */
     public function toArray(): array;
