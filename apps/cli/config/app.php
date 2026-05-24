bf8557f07c2176c78236b219e5bccf5bdccf6fb6
nckrtl <nick.retel@gmail.com>
2026-05-24 17:17:08 +0200

    feat(cli): scaffold apps/cli Laravel Zero app and widen core illuminate constraint (todo 373)

---
+++ apps/cli/config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name' => 'Orbit',
+    'version' => '0.1.0',
+    'env' => env('APP_ENV', 'development'),
+    'providers' => [],
+    'aliases' => [],
+];
