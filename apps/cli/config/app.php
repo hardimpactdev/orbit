da1e0938ef7d19490517a5f6872783e5d8e601a9
nckrtl <nick.retel@gmail.com>
2026-06-17 09:55:35 +0200

    Bump Orbit version to 0.1.3

--- apps/cli/config/app.php
+++ apps/cli/config/app.php
@@ -7,7 +7,7 @@ use App\Providers\OperationTokenGuardServiceProvider;
 
 return [
     'name' => 'Orbit',
-    'version' => '0.1.2',
+    'version' => '0.1.3',
     'env' => env('APP_ENV', 'development'),
     'providers' => [
         GatewayApiServiceProvider::class,
